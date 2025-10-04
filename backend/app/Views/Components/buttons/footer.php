c26d6b1f496839f231a22231048feecce2270399
Vian Tibay <202310591@fit.edu.ph>
2025-10-05 04:38:01 +0800

    refactor(footer): Correct spacing in copyright notice

--- backend/app/Views/Components/buttons/footer.php
+++ backend/app/Views/Components/buttons/footer.php
@@ -56,6 +56,6 @@
                   
          
          <!-- <div class="container mx-auto px-4 py-4 text-center text-gray-400 text-sm">
-              &copy; <?= date('Y') ?> Vian Tibay Project AD-Project. All rights reserved.
+              &copy;  Vian Tibay Project AD-Project. All rights reserved.
           </div>
            -->
