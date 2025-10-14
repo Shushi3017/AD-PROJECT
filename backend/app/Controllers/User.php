a1740f36528b04c7ae0425be10b227658d8aba1a
shushi3017 <vdtibay@fit.edu.ph>
2025-10-14 19:59:38 +0800

    refactor(routes): Update routes to remove landing page and direct to signup

--- backend/app/Controllers/User.php
+++ backend/app/Controllers/User.php
@@ -6,9 +6,9 @@ use App\Controllers\BaseController;
 
 class User extends BaseController
 {
-    public function landing(): string
+    public function signup(): string
     {
-        return view('user/landing');
+        return view('user/signup');
     }
   
 
